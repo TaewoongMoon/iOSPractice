df2214a539c7ad268d57c3bcaba7f5579ebfaf1f
TaewoongMoon <taewoongmoon1@gmail.com>
2021-10-27 15:05:16 +0900

    Add: First Button Creation Practice

---
+++ CWC14DayBeginnerChallenge/CWC14DayBeginnerChallenge/CWC14DayBeginnerChallengeApp.swift
@@ -0,0 +1,27 @@
+//
+//  CWC14DayBeginnerChallengeApp.swift
+//  CWC14DayBeginnerChallenge
+//
+//  Created by 문태웅 on 2021/10/27.
+//
+
+import SwiftUI
+
+@main
+struct CWC14DayBeginnerChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+//Body-r2
+
+struct Hello : App{
+    var body: some Scene{
+        WindowGroup{
+            ContentView()
+        }
+    }
+}
---
+++ CWC14DayBeginnerChallenge/CWC14DayBeginnerChallenge/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CWC14DayBeginnerChallenge
+//
+//  Created by 문태웅 on 2021/10/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World!").padding().background(Color.green).cornerRadius(10.0)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
